fbaf336427eb435ac0c295ff405ac5e864f91d4c
himanshi-2002 <72788591+himanshi-2002@users.noreply.github.com>
2020-10-13 19:08:07 +0530

    Day 2 work

---
+++ Day2_B29.py
@@ -0,0 +1,28 @@
+first_name = "Himanshi"
+print(first_name)
+
+firstname = "himanshi"
+print(firstname)
+
+name1 = "himanshi"
+print(name1)
+
+name="SAKSHI"
+print(name)
+
+print(type(name))
+
+name = 'anjali'
+name2 = "riya"
+name3 = '''pankaj'''
+print(name)
+print(name2)
+print(name3)
+
+print(type(name3))
+
+full_name = "himanshi kataria"
+print(full_name)
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
