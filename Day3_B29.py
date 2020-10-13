af42245a3a6d72ac3cf01e0ad6a8035dd39e0239
himanshi-2002 <72788591+himanshi-2002@users.noreply.github.com>
2020-10-14 01:09:22 +0530

    Day3 work

---
+++ Day3_B29.py
@@ -0,0 +1,32 @@
+full_name = "himanshi"
+print(full_name)
+
+name = "  python"
+print(name)
+
+name1 = "python  "
+print(name)
+
+name2 = "  python  "
+print(name)
+
+print(name.lstrip())
+print(name1.rstrip())
+print(name2.strip())
+
+print("Computer Languages : Python,C++,Java,Java 8")
+
+print("Computer Languages : \nPython\nC++\nJava\nJava 8")
+
+print("Computer Languages : \n\tPython\n\tC++\n\tJava\n\tJava 8")
+
+first_name = "sakshi"
+last_name = "kataria"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(full_name.title())
+
+full_name = f"Hello {first_name} {last_name}"
+print(full_name)
+print(full_name.title())
+
