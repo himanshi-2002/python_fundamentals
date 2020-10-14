37df3bc30ae8eaeedc7594cd73d90e71825e938d
himanshi-2002 <72788591+himanshi-2002@users.noreply.github.com>
2020-10-15 00:43:12 +0530

    Add files via upload

---
+++ Day4_B29.py
@@ -0,0 +1,50 @@
+students = ["sakshi","himanshi","vijay","bhushan","sachin"]
+print(students)
+
+print(students[1])
+print(students[4])
+
+print(type(students))
+
+print(students)
+print(students[3].title())
+
+students.append("ajeet")
+print(students)
+
+students.append("divya")
+print(students)
+
+students.insert(3,"jyoti")
+print(students)
+
+print(students[3])
+
+students.insert(5,"manju")
+print(students)
+
+print(students[5])
+
+students[0] = "GIRDHARI"
+print(students)
+
+del students[5]
+print(students)
+
+students.append("nikhita")
+print(students)
+
+students.insert(7,"rajeev")
+print(students)
+
+students[2] ="manju"
+print(students)
+
+del students[6]
+print(students)
+
+print(students)
+
+print(students[8])
+
+print(students[-2])
