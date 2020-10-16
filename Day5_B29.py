4b393ab602775260a57080f2b42197e821aaa746
himanshi-2002 <72788591+himanshi-2002@users.noreply.github.com>
2020-10-16 21:56:49 +0530

    Day5 work

---
+++ Day5_B29.py
@@ -0,0 +1,27 @@
+students = ['gunjan','sakshi','himanshi','bhushan','vijay','raju','arjun']
+print(students)
+
+print(type(students))
+
+print(sorted(students))
+
+print(students)
+
+students.sort()
+print(students)
+
+print(students)
+students.reverse()
+print(students)
+
+print(students)
+
+print(len(students))
+
+new = ['sakshi','himanshi','vijay','bhushan','raju','manju','jyoti','anil']
+print(new[0:2])
+print(new[2:4])
+print(new[4:6])
+print(new[6:8])
+
+print(new)
