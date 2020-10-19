a19d4abf4abb2939e3cfae2634e10fb17fb144a9
himanshi-2002 <72788591+himanshi-2002@users.noreply.github.com>
2020-10-20 00:12:21 +0530

    Day 7 work

---
+++ Day7_B29.py
@@ -0,0 +1,23 @@
+students = ['himanshi','sakshi','vijay','bhushan','suchi','ruchi','jyoti']
+
+for x in students:
+      print(f"Great job done ,{x} " )
+
+for marks in students:
+    print(f"Good job done , {marks}")
+    print(f"Please let me in case u have any doubt , {marks} \n ")
+
+for marks in students:
+    print(f"Good job done , {marks}")
+    print(f"Please let me in case u have any doubt , {marks} \n ")
+print("SEEKING FOR FUTHER RESPONSE....")
+
+print("INTRODUCTION TO TUPLE")
+
+dimensions = (200,250,300)
+print(dimensions)
+
+print(type(dimensions))
+
+dimensions = (150,200,250)
+print(dimensions)
