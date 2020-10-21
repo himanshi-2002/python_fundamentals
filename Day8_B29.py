248556fad98bd564595bb74964459adca2f78574
himanshi-2002 <72788591+himanshi-2002@users.noreply.github.com>
2020-10-21 20:42:02 +0530

    Day 8 work

---
+++ Day8_B29.py
@@ -0,0 +1,28 @@
+dimensions =(200,250)
+print(dimensions)
+
+print(type(dimensions))
+
+new = {"color" : "blue","code" : '50'}
+print(new)
+
+print(type(new))
+
+new['initial_value'] = 20
+new["final_value"] = 30
+
+print(new)
+
+print(new['color'])
+print(new["final_value"])
+
+print("FACEBOOK DETAILS")
+
+user = {"username":"Himanshikataria16","fname":"himanshi","lname":"kataria","password":"42321","age":"18"}
+print(user)
+
+user["location"] ="new delhi"
+print(user)
+
+user["password"] = "77775555"
+print(user)
